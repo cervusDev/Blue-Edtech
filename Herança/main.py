c3c29b7f68b0792f2030b880c63e3a043acd3fa7
gucervus <gustavo.cervus@gmail.com>
2021-05-28 16:28:21 -0300

    commit

--- Herança/main.py
+++ Herança/main.py
@@ -3,6 +3,25 @@ from lawyer import Lawyer
 
 doctorList = []
 lawyerList = []
+#                          crm      nome      cpf       telefone
+FirstPersonDoctor = Doctor(123456, 'Gustavo', 24787839, 243903)
+SecondPersonDoctor = Doctor(12321, 'Adwaita', 23434223, 234444)
 
-PersonDoctor = Doctor()
-PersonLawyer = Lawyer()
+doctorList.append(FirstPersonDoctor)
+doctorList.append(SecondPersonDoctor)
+
+#                          oab    name     cpf     telefone
+FirstPersonLawyer = Lawyer(2343, 'Hector', 345435, 5435534)
+SecondPersonLawyer - Lawyer(2345, 'Ricardo', 1233, 3213213)
+
+lawyerList.append(FirstPersonLawyer)
+lawyerList.append(SecondPersonLawyer)
+
+for law in lawyerList:
+    print('OAB: {} | NAME: {} | AGE: {}'.format(law.getOab(), law.getName(), law.getAge()))
+    print('CPF: {} | PHONE: {}'.format(law.getCpf(), law.getPhone().getPhone())),end = '\n\n'
+    
+for med in doctorList:
+    print('CRM: {} | NAME: {} | AGE: {}'.format(med.getOab(), med.getName(), med.getAge()))
+    print('CPF: {} | PHONE: {}'.format(med.getCpf(), med.getPhone().medPhone())),end = '\n\n'
+    
