8df86d3426a0f0bc69a581260ac8f1e55c0ef4ac
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-06-10 20:40:16 -0300

    atualizaçao de palestra

--- 2° Módulo - Banco de Dados/A3_Flask/E8_Humor/app.py
+++ 2° Módulo - Banco de Dados/A3_Flask/E8_Humor/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+image = True
 
 @app.route('/')
 def home():
+    luffyBolado = 'https://i.pinimg.com/originals/50/c5/f1/50c5f1847013012ee0f25f67fdddb8d9.gif'
+
     return render_template(
     'index.html',
+    luffyBolado = luffyBolado,
+    image = image
+    )
+
+@app.route('/troca', methods = ['POST'])
+def troca():
+    image = False
+    luffySuave = 'https://media.tenor.com/images/9ff03b679bf96782b7ff1fb4089ad7e9/tenor.gif'
+
+    return render_template(
+    'index.html',
+    image = image,
+    luffySuave = luffySuave,
     )
 
 if __name__ == '__main__':
