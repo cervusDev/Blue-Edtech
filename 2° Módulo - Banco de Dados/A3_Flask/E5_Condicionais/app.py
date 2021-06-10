8307cb6466f78ed17ed45f37d6c951cd27a2eae8
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-06-10 11:44:28 -0300

    atualizaçao das aulas

--- 2° Módulo - Banco de Dados/A3_Flask/E5_Condicionais/app.py
+++ 2° Módulo - Banco de Dados/A3_Flask/E5_Condicionais/app.py
@@ -4,18 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    name = 'Badass Sonic'
-    hp = 2000
-    
+    luffy_name = 'Monk D. Luffy'
+    kaido_name = 'Kaido das Feras'
+
     exibir_imagem = True
-    imagem = 'https://i.pinimg.com/originals/82/81/4e/82814e4a976d84271a133821984bdc87.png'
+
+    luffy = 'https://i.pinimg.com/originals/17/dc/31/17dc31b25944a72c94ef40bd121dd66c.gif'
+    kaido = 'https://i.pinimg.com/originals/48/79/f5/4879f5159b4905d90f03bf591e16b074.gif'
     
     return render_template(
     'index.html',
-    name = name,
-    hp = hp,
     exibir_imagem = exibir_imagem,
-    imagem = imagem
+    luffy_name = luffy_name,
+    kaido_name = kaido_name,
+    luffy = luffy,
+    kaido = kaido,
     )
 
 if __name__ == '__main__':
