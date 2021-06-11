db1f639295798a167375830c78631bcd2fca8b0f
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-06-11 17:59:37 -0300

    Modulo em andamento

--- 2° Módulo - Banco de Dados/A4_CodeLab/E8_Humor/app.py
+++ 2° Módulo - Banco de Dados/A4_CodeLab/E8_Humor/app.py
@@ -6,22 +6,28 @@ image = True
 @app.route('/')
 def home():
     luffyBolado = 'https://i.pinimg.com/originals/50/c5/f1/50c5f1847013012ee0f25f67fdddb8d9.gif'
-
+    nome = 'Nome: Monk D. Luffy'
+    humor = 'Humor: Luffy está '
     return render_template(
     'index.html',
     luffyBolado = luffyBolado,
-    image = image   
+    image = image,
+    nome = nome ,
+    humor = humor
     )
 
 @app.route('/troca', methods = ['POST'])
 def troca():
     image = False
-    luffySuave = 'https://media.tenor.com/images/9ff03b679bf96782b7ff1fb4089ad7e9/tenor.gif'
-
+    luffySuave = 'https://i.pinimg.com/originals/02/0c/e7/020ce7e3b883d974e73d9fdffb4f20ad.gif'
+    nome = 'Nome: Monk D. Luffy'
+    humor = 'Humor: Luffy está '
     return render_template(
     'index.html',
     image = image,
     luffySuave = luffySuave,
+    nome = nome ,
+    humor = humor
     )
 
 if __name__ == '__main__':
