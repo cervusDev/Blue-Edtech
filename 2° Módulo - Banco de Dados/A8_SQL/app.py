f70b2b7538094af64b876c95f421ae20bf5568fc
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-06-30 16:49:32 -0300

    rota de visualizaçao concluida

--- 2° Módulo - Banco de Dados/A8_SQL/app.py
+++ 2° Módulo - Banco de Dados/A8_SQL/app.py
@@ -1,9 +1,10 @@
 from flask import (Flask, render_template)
 from flask_sqlalchemy import (SQLAlchemy)
 
+#Inicialização do Flask
 app = Flask(__name__)
 
-#DataBase constructor
+# Configurando o banco de dados
 user = 'xbjebcjp'
 password = 'jgQSsvr7VQeW2k1nJbSxirnjNi0Dijhj'
 host = 'tuffi.db.elephantsql.com'
@@ -13,23 +14,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'adwaita'
 
+#Inicialização do banco de dados
 db = SQLAlchemy(app)
 
-#Filmes
-
+#Modelando o banco de dados
 class Filmes(db.Model):
   id = db.Column(db.Integer, primary_key = True)
-  nome = db.Column(db.String(255), nullable = False)
+  nome   = db.Column(db.String(255), nullable = False)
   url = db.Column(db.String(255), nullable = False)
 
   def __init__(self, nome, url):
     self.nome = nome
     self.url = url
-  
+
+  # Conversor de função para método: lendo todos os elementos
   @staticmethod
   def read_all():
-    return Filmes.query.order_by(Filmes.id.desc()).all()
+    #select * from filmes order by id asc
+    #query = select 
+    return Filmes.query.order_by(Filmes.id.asc()).all()
+  
+  @staticmethod
+  def resume(registro_id):
+    #select * from filmes where id == xpto
+    return Filmes.query.get(registro_id) # Este parâmetro registro_id está ligado ao parâmetro da função details
 
+
+#Rotas
 @app.route('/')
 def home ():
   return render_template(
@@ -47,10 +58,28 @@ def read ():
 
 @app.route('/read/<registro_id>')
 def details (registro_id):
-  return 'pagina do filme' + registro_id  
+  image = ''
+  registro = Filmes.resume(registro_id)
+  print(registro)
 
+  if registro_id == 1:
 
-# trabalhar com dados mockados
+    image = 'https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG'
+
+  elif registro_id == 2:
+
+    image = 'https://images-na.ssl-images-amazon.com/images/I/61gZbAKOU5L._AC_SX522_.jpg'
+
+  elif registro_id == 3:
+
+    image = 'https://upload.wikimedia.org/wikipedia/pt/9/94/Matrix_revolutions.jpg'
+
+  return render_template(
+    'resume.html',
+    image = image,
+    registro = registro
+  ) 
+
 
 if __name__ == '__main__':
   app.run(debug = True)    
