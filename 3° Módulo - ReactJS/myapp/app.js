40d9909f6a2c01c52c028923682660a1c586ea69
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-07-12 17:09:29 -0300

    primeiro passo com node

---
+++ 3° Módulo - ReactJS/myapp/app.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator') // ECMAS 5 
+
+console.log(calculator.sum(10,20))
+console.log(calculator.sum(10,20))
+console.log(calculator.mult(10,20))
+console.log(calculator.div(10,20))
---
+++ 3° Módulo - ReactJS/myapp/calculator.js
@@ -0,0 +1,26 @@
+const name = "Calculadora: 1.0.0"
+
+const sum = (a, b) => {
+  return a+b;
+};
+
+const sub = (a, b) => {
+  return a-b;
+};
+
+const mult = (a, b) => {
+  return a*b;
+};
+
+const div = (a, b) => {
+  return a/b;
+};
+
+module.exports = {
+  sum,
+  sub,
+  mult,
+  div,
+  name
+};
+
