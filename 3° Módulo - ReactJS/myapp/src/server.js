6be8f16416c7a0804959cc7d18ea1580fc2a6c24
gucervus <gustavo.cervus@gmail.com>
2021-07-14 18:08:03 -0300

    metodo post

--- 3° Módulo - ReactJS/myapp/src/server.js
+++ 3° Módulo - ReactJS/myapp/src/server.js
@@ -1,8 +1,33 @@
 const express = require("express")
 const app = express() 
+const port = 3000
+
+app.use(express.json())
+
+const meetings = [
+  'Aula 01',
+  'Aula 02',
+  'Aula 03',
+]
 
 app.get("/", (req, res) => {
-  res.send('servidor rodando')
+  res.send(meetings)
 })
 
-app.listen(3000, (error) => error === false ? console.log('erro no servidor'): console.log('servidor rodando!'))
+app.get("/:id", (req,res) => {
+  const id = req.params.id-1
+  const meeting = meetings[id]
+
+  !meeting ? res.send('Aula não encontrada'):res.send(meeting)
+})
+
+app.post("/meetings", (req, res) => {
+ const meeting = req.body.meeting
+ const id = meetings.length
+
+ meetings.push(meeting)
+ res.send(`filme ${meeting} com sucesso no id ${id+1}!`)
+
+})
+
+app.listen(port, (err) => !err ? console.log('servidor rodando!'): console.log('erro no servidor'))
